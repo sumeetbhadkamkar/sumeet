0a5f556dd51f875f4d108982e9aa43b254561fbc
sumeetbhadkamkar <123872544+sumeetbhadkamkar@users.noreply.github.com>
2023-01-29 17:27:21 +0530

    Add files via upload

---
+++ Basic.r
@@ -0,0 +1,35 @@
+1+1
+2+2
+a <- 10
+1+1
+b <<- 10
+10 -> c
+10 ->> d
+exp(1)
+sqrt(10)
+pi
+
+add = b+c
+# comment
+Vect_x <- c(10,20,30,40)
+Vect_y <- c(1,2,3,4)
+
+Vect_x+Vect_y
+
+Vect_y[2] <- 10
+df <- data.frame(x = 1:3 ,y =c("a","b","c"))
+v_char <- c('a','b','c',10,20,10.5)
+v_char
+Df_new <- data.frame(height=c(150,160),weight=c(65,72))
+df[1,2]
+df[1,1]
+df[1,1][1,2]
+df[1,c(1,2)]
+df[1,]
+c(df[1,2],df[3,2])
+c(df[1,1],df[3,1])
+c(df[1,1],df[1,2],df[3,1],df[3,2])
+c(df[1,],df[3,])
+x <- df[1,]
+y <- df[3,]
+c(x,y)
